a8b64ff1f17bda8721e1e5c495b0ce849c07fa69
dgstjohn <dgstjohn@gmail.com>
2021-08-31 23:28:42 -0400

    added POST route to create new Batter if not exists

--- controllers/api/batters-routes.js
+++ controllers/api/batters-routes.js
@@ -1,66 +1,97 @@
 const router = require('express').Router();
 const { Batters } = require('../../models');
 
+
+
 //api/batters-route/
 
 //get all batters
 router.get('/', (req, res) => {
-    Batters.findAll({
+  Batters.findAll({
     attributes: [
-        'id',
-        'name',
-        'team',
-        'games_played',
-        'hits',
-        'average',
-        'home_runs',
-        'rbi'
-      ],
+      'id',
+      'name',
+      'team',
+      'games_played',
+      'hits',
+      'average',
+      'home_runs',
+      'rbi'
+    ],
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(data);
     })
-      .then(data => {
-        if (!data) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(data);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //get batter by id
 router.get('/:id', (req, res) => {
-    Batters.findOne({
-      where: {
-        id: req.params.id
-      },
-      attributes: [
-        'id',
-        'name',
-        'team',
-        'games_played',
-        'hits',
-        'average',
-        'home_runs',
-        'rbi'
-      ],
+  Batters.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'name',
+      'team',
+      'games_played',
+      'hits',
+      'average',
+      'home_runs',
+      'rbi'
+    ],
+  })
+    .then(data => {
+      if (!data) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      const batter = data.get({ plain: true });
+      res.render('homepage', { batter, plain: true });
+      //console.log(data);
+      //res.json(data);
     })
-      .then(data => {
-        if (!data) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        const batter = data.get({ plain: true });
-        res.render('homepage', {batter, plain: true});
-        //console.log(data);
-        //res.json(data);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-  module.exports = router;
+// add new batter to DB if they're not already in there
+router.post('/', (req, res) => {
+  Batter.findOrCreate({
+    where: {
+      name: req.body.name
+    },
+    defaults: {
+      name: req.body.name,
+      team: req.body.team,
+      games_played: 0,
+      hits: 0,
+      average: .000,
+      home_runs: 0,
+      rbi: 0
+    }
+  })
+    .then(data => {
+      if (req.body.name = true) {
+        res.status(409).json({ message: 'Player is already in our database -- search for him at the top!' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
