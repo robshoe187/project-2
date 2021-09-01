8921206c91cedb4cf579659cf4d5349bbb1ee5b9
dgstjohn <dgstjohn@gmail.com>
2021-08-31 21:48:43 -0400

    still needs array push, committing to free up branch

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const addRoutes = require('./add-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/add', addRoutes);
 
 module.exports = router;
