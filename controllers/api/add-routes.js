1e0897f3b4a29ad75cce4af691180e9553c4e7b9
dgstjohn <dgstjohn@gmail.com>
2021-08-31 22:00:55 -0400

    another pro forma add

--- controllers/api/add-routes.js
+++ controllers/api/add-routes.js
@@ -11,9 +11,8 @@ router.post('/', (req, res) => {
         home_runs: 0,
         rbi: 0
     })
-        .then(dbUserData => {
-            res.json(dbUserData);
-            // array push
+        .then(data => {
+            res.json(data);
         });
     .catch(err => {
         console.log(err);
