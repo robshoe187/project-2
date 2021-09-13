484d40597e093330e6fc50503523197860a017b2
dgstjohn <dgstjohn@gmail.com>
2021-09-12 20:50:20 -0400

    repo cleanup

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
-const postRoutes = require('./post-routes');
-const commentRoutes = require('./comment-routes');
 const batterRoutes = require('./batters-routes.js');
-// const addRoutes = require('./add-routes.js');
 
 router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/comments', commentRoutes);
 router.use('/batters', batterRoutes);
-// router.use('/add', addRoutes);
 
 module.exports = router;
